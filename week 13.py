f3251f1376e6c4c4a52e9fd6b6f0fb78b7abb6de
박유진 <qkrdbw@naver.com>
2023-11-13 21:09:46 +0900

    14426번: 접두사 찾기 (이진탐색)

--- week 13.py
+++ week 13.py
@@ -18,3 +18,50 @@ def solution(scoville, K):
         answer += 1
 
     return answer
+
+# 14426번: 접두사 찾기 (이진탐색)
+def binary_search(n_list, prefix):
+    left, right = 0, len(n_list) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if n_list[mid].startswith(prefix):
+            return True
+        elif n_list[mid] < prefix: # 알파벳 순서
+            left = mid + 1 # 검색 범위가 오른쪽으로 좁혀짐
+        else:
+            right = mid - 1 # 검색 범위가 왼쪽으로 좁혀짐
+
+    return False
+
+n, m = map(int, input().split())
+n_list = [input() for _ in range(n)]
+m_list = [input() for _ in range(m)]
+
+n_list.sort()
+
+cnt = 0
+for prefix in m_list:
+    if binary_search(n_list, prefix):
+        cnt += 1
+
+print(cnt)
+
+# 시간초과
+n, m = map(int, input().split())
+
+n_list = [input() for _ in range(n)]
+m_list = [input() for _ in range(m)]
+	
+def prefix(n_list, m_list):
+	cnt = 0
+	
+	for i in m_list:
+		for j in n_list:
+			if j.startswith(i):
+				cnt += 1
+				break
+	
+	return cnt
+
+print(prefix(n_list, m_list))
