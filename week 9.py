84dc389c0075ca069549fe1a6782c5cfc3769f73
박유진 <qkrdbw@naver.com>
2023-11-13 21:02:52 +0900

    프로그래머스: 해킹 (다익스트라)

--- week 9.py
+++ week 9.py
@@ -16,3 +16,43 @@ def solution(N, road, K):
             answer += 1
 
     return answer
+
+# 프로그래머스: 해킹 (다익스트라)
+import sys
+input = sys.stdin.readline
+import heapq
+
+t = int(input())
+inf = float('inf')
+
+def dijkstra(N, D, C):
+    g = [[] for _ in range(N + 1)]
+    for _ in range(D):
+        a, b, s = map(int, input().split())
+        g[b].append((a, s))
+
+    dist = [inf for _ in range(N + 1)]
+    dist[C] = 0
+
+    h = [(0, C)] 
+    time = 0
+    cnt = 0
+
+    while h:
+        cost, node = heapq.heappop(h)
+        if dist[node] >= cost:
+            for n, c in g[node]:
+                newdist = dist[node] + c
+                if newdist < dist[n]:
+                    dist[n] = newdist
+                    heapq.heappush(h, (newdist, n))
+    
+    reachable_distances = [dist[i] for i in range(1, N+1) if dist[i] != inf]
+    cnt = len(reachable_distances)
+    time = max(reachable_distances) if reachable_distances else 0
+    
+    return f"{cnt} {time}"
+
+for _ in range(t):
+    n, d, c = map(int, input().split())
+    print(dijkstra(n, d, c))
