6ead8730702ad6f38cb7cb4db46fe5a0ddfa0307
박유진 <qkrdbw@naver.com>
2023-11-13 20:32:53 +0900

    13414번: 수강신청

--- week 3.py
+++ week 3.py
@@ -22,3 +22,23 @@ def trap(h, w, height):
 h, w = map(int, input().split())
 height = list(map(int, input().split()))
 print(trap(h, w, height))
+
+# 13414번: 수강신청
+import sys
+input = sys.stdin.readline
+
+k, l = map(int, input().split())
+s = dict()
+
+for i in range(1, l+1):
+    num = input().rstrip()
+    s[num] = i
+    
+s = dict(sorted(s.items(), key = lambda x:x[1]))
+
+cnt = 0
+for i in s.keys():
+    cnt += 1
+    print(i)
+    if cnt == k:
+        break
