a90070cabc7ed9b13c8fb9a7f641a8a31b4a96c6
박유진 <qkrdbw@naver.com>
2023-11-13 18:35:31 +0900

    2230번: 수고르기 (슬라이딩윈도우)

--- week 15.py
+++ week 15.py
@@ -1,4 +1,4 @@
-# 12847번: 꿀아르바이트
+# 12847번: 꿀아르바이트 (슬라이딩윈도우)
 n, m = map(int, input().split())
 pay = list(map(int, input().split()))
 
@@ -15,6 +15,30 @@ def maximum_profit(n, m, pay):
 result = maximum_profit(n, m, pay)
 print(result)
 
+
+# 2230번: 수고르기 (슬라이딩윈도우)
+n, m = map(int, input().split())
+nums = [int(input()) for _ in range(n)]
+
+def get_minimum(n, m, nums):
+    nums.sort()
+    left, right = 0, 1
+    min_diff = float('inf')
+
+    while right < n:
+        diff = nums[right] - nums[left]
+        if diff >= m:
+            min_diff = min(min_diff, diff)
+            left += 1
+            if left == right:
+                right +=1
+        else:
+            right += 1
+
+    return min_diff
+
+result = get_minimum(n, m, nums)
+print(result)
 
 
 
