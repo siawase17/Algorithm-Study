7a42c67ed28a7cdfec71e5fc23110f398e2a4328
박유진 <qkrdbw@naver.com>
2023-11-13 20:31:31 +0900

    18311번: 왕복

--- week 2.py
+++ week 2.py
@@ -8,3 +8,16 @@ def isPalindrome(text):
 text = input()
 result = isPalindrome(text)
 print(result)
+
+# 18311번: 왕복
+n, k = map(int, input().split())
+course = list(map(int, input().split()))
+return_course = course + course[::-1]
+
+a = 0
+for i in range(n * 2):
+    a += return_course[i]
+    if a > k:
+        break
+        
+print(i+1 if i < n else 2*n - i)
