0e5c9daa84c30941db1a31b2091c002b12bf4c77
박유진 <qkrdbw@naver.com>
2023-11-22 13:45:49 +0900

    11501번: 주식 (그리디)

--- week 16.py
+++ week 16.py
@@ -1,14 +1,14 @@
 # 11501번: 주식 (그리디)
 t = int(input())
+
 def max_profit(t):
     for _ in range(t):
         n = int(input())
         arr = list(map(int, input().split()))
-        arr.reverse()
-        current = arr[0]
+        current = arr[n - 1]
         profit = 0
 
-        for i in range(1, n):
+        for i in range(n - 2, -1, -1):
             if current < arr[i]:
                 current = arr[i]
                 continue
