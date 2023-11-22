fa35a85104c03ce3e32a817b69a286d9b7d27ad7
박유진 <qkrdbw@naver.com>
2023-11-22 13:23:32 +0900

    2141번: 우체국 (그리디)

--- week 16.py
+++ week 16.py
@@ -1,5 +1,4 @@
 # 11501번: 주식 (그리디)
-
 t = int(input())
 def max_profit(t):
     for _ in range(t):
@@ -18,6 +17,61 @@ def max_profit(t):
         print(profit)
 
 max_profit(t)
+
+# 2141번: 우체국 (그리디)
+n = int(input())
+town = [] # (위치, 인구)
+population = 0  # 전체 인구 수
+
+# 각 마을 정보 입력 및 전체 인구 수 누적
+for _ in range(n):
+    pos, p = map(int, input().split())
+    town.append((pos, p))
+    population += p
+
+# 마을을 위치를 기준으로 정렬
+town.sort()
+
+# 전체 인구 수의 중앙 위치 구하기
+mid_population = (population + 1) // 2
+
+# 인구의 절반이 속한 마을에 우체국 설치
+cur_population = 0
+for loc, p in town:
+    cur_population += p
+
+    if cur_population >= mid_population:
+        print(loc)
+        break
+
+####### 아래는 함수 풀이 ###########
+
+def find_location(town):
+    population = sum(map(lambda x: x[1], town)) # 전체 인구 수
+    town.sort() # 마을을 위치를 기준으로 정렬
+
+    # 전체 인구 수의 중앙 위치 구하기
+    mid_population = (population + 1) // 2
+
+    # 인구의 절반이 속한 마을에 우체국 설치
+    cur_population = 0
+    for loc, p in town:
+        cur_population += p
+
+        if cur_population >= mid_population:
+            return loc
+
+n = int(input())
+town = [] # (위치, 인구)
+
+# 각 마을 정보 입력
+for _ in range(n):
+    pos, p = map(int, input().split())
+    town.append((pos, p))
+
+result = find_location(town)
+print(result)
+
 
 
 
