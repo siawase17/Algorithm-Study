be1a36a153be00a3c6de10d8b2241439a6a639f7
Yujin <100908994+siawase17@users.noreply.github.com>
2024-02-03 01:01:06 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = [n];
+    while (n !== 1) {
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else {
+            n = n * 3 + 1;
+        }
+        answer.push(n);
+    }
+    return answer;
+}
